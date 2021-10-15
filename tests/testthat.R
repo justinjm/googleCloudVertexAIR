004de0646575c5176173e38f96d41150f2b4f94b
Justin Marciszewski <justin@harborislandanalytics.com>
2021-10-15 14:36:21 -0400

    adding integration test for listing datasets function

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(googleCloudAVertexAIR)
+
+test_check("googleCloudAVertexAIR")
---
+++ tests/testthat/test-gcva.R
@@ -0,0 +1,32 @@
+context("Setup")
+
+options(googleAuthR.scopes.selected = "https://www.googleapis.com/auth/cloud-platform")
+googleAuthR::gar_auth_service(json_file = Sys.getenv("GAR_SERVICE_JSON"))
+
+skip_if_no_token <- function() {
+  testthat::skip_if_not(googleAuthR::gar_has_token(), "No token")
+}
+
+context("Authentication")
+
+test_that("Authentication", {
+  skip_if_no_token()
+
+  # manual auth
+  expect_true(googleAuthR::gar_has_token())
+})
+
+test_that("We can fetch a list of datasets", {
+  skip_if_no_token()
+
+  projectId <- Sys.getenv("GCVA_DEFAULT_PROJECT_ID")
+  locationId <- Sys.getenv("GCVA_DEFAULT_REGION")
+  expect_true(projectId != "")
+  expect_true(locationId != "")
+  l <- vair_list_datasets(projectId, locationId)
+
+  expect_s3_class(l, "data.frame")
+  expect_true(
+    all(names(l) %in% c("displayName", "createTime", "etag", "name"))
+  )
+})
