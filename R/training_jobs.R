93ad3880348b122db21e9df3b8dae81847b89f8c
Justin Marciszewski <justin@harborislandanalytics.com>
2022-07-08 16:36:21 -0400

    work on #5: tabular automl training job construction function

--- R/training_jobs.R
+++ R/training_jobs.R
@@ -1,34 +1,34 @@
 #'
-#' @param projectId GCP project id
-#' @param locationId location of GCP resources
 #' @param displayName STRING Required. The user-defined name of this TrainingPipeline.
 #'
 #' @export
-gcva_automl_tabluar_training_job <- function(projectId = gcva_project_get(),
-                                             locationId = gcva_region_get(),
-                                             displayName) {
+gcva_automl_tabluar_training_job <- function(
+  displayName,
+  optimizationPredictionType = c("regression", "classification"),
+  transformations=NULL,
+  budgetMilliNodeHours=1000) {
+
+  # set prediction type from available list
+  optimizationPredictionType <- match.arg(optimizationPredictionType)
+
 
   request_body <- structure(
     list(
       displayName = displayName,
       # inputDataConfig = list(), # below in job Run?
       trainingTaskDefinition = "gs://google-cloud-aiplatform/schema/trainingjob/definition/automl_tabular_1.0.0.yaml",
-      # trainingTaskInputs = list(
-      #   targetColumn = ,
-      #   predictionType = ,
-      #   transformations = list(),
-      #   budgetMilliNodeHours
-      # )
-
-
-
-    )
+      trainingTaskInputs = list(
+        targetColumn = c("column_name"),
+        predictionType = optimizationPredictionType,
+        transformations = list(
+          transformations
+        ),
+        budgetMilliNodeHours = budgetMilliNodeHours
+      )
+    ), class = c("gcva_automl_tabluar_training_job", "list")
   )
 
-  out <- request_body
-
-  structure(out, class = "gcva_automl_tabluar_training_job")
-
+  request_body
 
 }
 
@@ -55,5 +55,12 @@ gcva_run_job <- function(job,
     stop(sprintf("Dataset %s does not exist. Please check the dataset displayname is correct and try again.",
                  displayName))
   }
+
+  request_body <- structure(
+    list(
+      # inputDataConfig = list(), # below in job Run?
+    )
+
+  )
 
 }
