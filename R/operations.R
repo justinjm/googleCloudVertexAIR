561c107d3525ec32b5739d17b69acc6c784a6eba
Justin Marciszewski <justin@harborislandanalytics.com>
2022-11-20 13:19:02 -0500

    placeholder for proper implementation of operations method
    pre-work for #6

---
+++ R/operations.R
@@ -0,0 +1,16 @@
+
+# https://github.com/MarkEdmondson1234/bigQueryR/blob/master/R/jobs.R
+
+#'
+gcva_wait_op <- function(job,
+                         wait=5) {
+
+}
+
+
+#'
+gcva_get_op <- function(projectId,
+                        operationId) {
+
+
+}
