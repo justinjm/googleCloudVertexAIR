7488a68917117ea8171f9c6e6ffc6337df83acbe
Justin Marciszewski <justin@harborislandanalytics.com>
2022-11-20 23:44:45 -0500

    initial test of trainingPipeline job method to properly handle response

--- R/training_jobs.R
+++ R/training_jobs.R
@@ -44,24 +44,31 @@ gcva_automl_tabluar_training_job <- function(
 #' Executes a custom container training job
 #'
 #'
-#' @param
 #'
 #'
 #'
 #'
-#' @export
 # gcva_custom_container_training_job <- function(){
 #
 # }
 
 
 #' Executes an training job
+#' https://cloud.google.com/vertex-ai/docs/reference/rest/v1/projects.locations.trainingPipelines/create
+#'
 #' @param projectId
 #' @param locationId
 #' @param job
 #' @param dataset
-#' @param
+#' @param targetColumn STRING
+#' @param trainingFractionSplit
+#' @param validationFractionSplit
+#' @param testFractionSplit
+#' @param modelDisplayName
+#' @param disableEarlyStopping
+#' @param wait wait for model training to complete before completing function
 #'
+#' @returns model object
 #'
 #' @export
 gcva_run_job <- function(projectId = gcva_project_get(),
@@ -73,14 +80,13 @@ gcva_run_job <- function(projectId = gcva_project_get(),
                          validationFractionSplit=0.1,
                          testFractionSplit=0.1,
                          modelDisplayName,
-                         disableEarlyStopping=FALSE){
+                         disableEarlyStopping=FALSE,
+                         wait=TRUE){
 
   # check if dataset object
   stopifnot(inherits(dataset, "gcva_dataset"))
 
-  # get dataset ID from url since not sure how else?
-  # https://cloud.google.com/apis/design/resource_names
-  # dataset_id <- gsub(".*/datasets/" , "", dataset$name)
+  # get dataset ID from datasetBane uri
   dataset_id <- unlist(strsplit(dataset$name, "/"))[6]
 
   request_body_partial <- structure(
@@ -116,12 +122,85 @@ gcva_run_job <- function(projectId = gcva_project_get(),
 
   stopifnot(inherits(TrainingPipeline, "gcva_job"))
 
-  response <- f(the_body = TrainingPipeline)
+  # trainingPipeline <- f(the_body = TrainingPipeline)
 
-  out <- structure(response, class = c("gcva_trainingPipeline"))
+  trainingPipeline <- gcva_wait_for_training_pipeline(trainingPipelineName = trainingPipeline$name)
+  out <- gcva_trainingPipeline(trainingPipelineName = trainingPipeline$name)
+
+  out
+
+  # structure(trainingPipeline, class = c("gcva_trainingPipeline"))
+}
+
+
+#' Wait for a training pipeline operation
+#'
+#' @param locationId locationId of training pipeline
+#' @param trainingPipelineName an object representing a model training pipeline
+#' @param wait INTEGER number of seconds to wait between checks. Default is 5minutes
+#'
+#' @return trainingPipeline object
+#'
+#' @export
+gcva_wait_for_training_pipeline <- function(locationId = gcva_region_get(),
+                                            trainingPipelineName,
+                                            wait=5) {
+
+  tp_obj <- gcva_trainingPipeline(trainingPipelineName = trainingPipelineName)
+  trainingPipelineId <- unlist(strsplit(tp_obj$name, "/"))[6]
+  tp_console_url <- sprintf(
+    "https://console.cloud.google.com/ai/platform/locations/%s/training/%s?project=%s",
+    locationId, trainingPipelineId, projectId)
+
+  myMessage("view training:\n", tp_console_url, level = 3)
+
+  status <- FALSE
+  time <- Sys.time()
+
+  while(!status){
+    Sys.sleep(wait)
+    tp <- gcva_trainingPipeline(trainingPipelineName = trainingPipelineName)
+
+    myMessage("view training:\n", tp_console_url, level = 3)
+    myMessage("pipeline state: ", tp$state, level = 3)
+
+    if(tp$state == "PIPELINE_STATE_SUCCEEDED" |
+       tp$state == "PIPELINE_STATE_FAILED" |
+       tp$state == "PIPELINE_STATE_CANCELLED"){
+      status <- TRUE
+    } else {
+      status <- FALSE
+    }
+  }
+  tp
+}
+
+
+#' Get a Training Pipeline object
+#' https://cloud.google.com/vertex-ai/docs/reference/rest/v1/projects.locations.trainingPipelines/get
+#'
+#' @param locationId locationId of training pipeline
+#' @param trainingPipelineName trainingPipeline full name or uri
+#'
+#' @return a training Pipeline object
+#'
+#' @export
+gcva_trainingPipeline <- function(locationId = gcva_region_get(),
+                                  trainingPipelineName) {
+
+  url <- sprintf("https://%s-aiplatform.googleapis.com/v1/%s",
+                 locationId,
+                 trainingPipelineName)
+
+  f <- googleAuthR::gar_api_generator(url,
+                                      "GET",
+                                      data_parse_function = function(x) x,
+                                      checkTrailingSlash = FALSE)
+
+  response <- f()
+
+  out <- structure(response, class = "gcva_trainingPipeline")
 
   out
 
 }
-
-
