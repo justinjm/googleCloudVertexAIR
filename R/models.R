e3d44dded165b14e2d2dc4abe173be28b0403d77
Justin Marciszewski <justin@harborislandanalytics.com>
2022-11-21 00:40:15 -0500

    implemented get model function and test, updated vignette
    close #12

--- R/models.R
+++ R/models.R
@@ -33,3 +33,33 @@ gcva_list_models <- function(projectId = gcva_project_get(),
   out
 
 }
+
+
+#' Get a Model object
+#' https://cloud.google.com/vertex-ai/docs/reference/rest/v1/projects.locations.models/get
+#'
+#' @param locationId locationId of operation
+#' @param Model the full name of the model object to get
+#'
+#' @return a Model object
+#'
+#' @export
+gcva_model <- function(locationId = gcva_region_get(),
+                       modelName){
+
+  url <- sprintf("https://%s-aiplatform.googleapis.com/v1/%s",
+                 locationId,
+                 modelName)
+
+  f <- googleAuthR::gar_api_generator(url,
+                                      "GET",
+                                      data_parse_function = function(x) x,
+                                      checkTrailingSlash = FALSE)
+
+  response <- f()
+
+  out <- structure(response, class = "gcva_model")
+
+  out
+
+}
