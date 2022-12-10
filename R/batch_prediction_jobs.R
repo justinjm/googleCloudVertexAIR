9b95ce4072eab15c0388f71e0107c2baba3012eb
Justin Marciszewski <justin@harborislandanalytics.com>
2022-12-10 10:20:43 -0500

    first rough outline of request body and adding param checks
    work on #8

--- R/batch_prediction_jobs.R
+++ R/batch_prediction_jobs.R
@@ -1,49 +1,60 @@
 
+
+
+
+
+
+
 #'
-#'
+#' @param jobDisplayName STRING Required. The user-defined name of this BatchPredictionJob.
+#' @param model STRING The name of the Model resource that produces the predictions via this job,
+#' must share the same ancestor Location. Starting this job has no impact on any existing deployments of the Model and their resources.
+#' Exactly one of model and unmanagedContainerModel must be set.
+#' The model resource name may contain version id or version alias to specify the version,
+#' if no version is specified, the default version will be used.
+#' @param bigquerySource BigQuery URI to a table, up to 2000 characters long. Accepted forms: "bq://projectId.bqDatasetId.bqTableId"
 #'
 #' https://cloud.google.com/vertex-ai/docs/reference/rest/v1/projects.locations.batchPredictionJobs#BatchPredictionJob
 gcva_batch_predict <- function(jobDisplayName,
                                model,
                                bigquerySource,
-                               instancesFormat,
-                               predictionsFormat,
+                               instancesFormat = c("bigquery"),
+                               predictionsFormat = c("bigquery"),
                                bigqueryDestinationPrefix) {
 
-  # what is default params to run function?
+  # TODO - what is default params to run function? bq or gcs?
+  # check bq url
+  tryCatch({
+    stopifnot(grepl("bq://", bigquerySource, fixed = TRUE))
+
+  }, error = function(e) {
+    stop("error: ", e$message)
+
+  })
 
 
-  # model
-  # check if
-  # Bq sourc
-  bigquerySource <- match.arg()
-  instancesFormat <- match.arg()
-  predictionsFormat <- match.arg()
+  instancesFormat <- match.arg(instancesFormat)
+  predictionsFormat <- match.arg(predictionsFormat)
 
 
   # build input config
-  # inputConfig =
-
-
+  inputConfig = list(
+    instancesFormat = instancesFormat,
+    bigquerySource = bigquerySource
+  )
 
   # build output config
-  # outputConfig =
+  outputConfig =list(
+    predictionsFormat = predictionsFormat
+  )
 
   # merge into request body
-
-
   request_body <- structure(
     rmNullObs(
       list(
-        displayName = displayName,
-        inputConfig = list(
-          instancesFormat = instancesFormat,
-          bigquerySource = bigquerySource
-        ),
-        outputConfig = list(
-          predictionsFormat = predictionsFormat
-
-        ),
+        displayName = jobDisplayName,
+        inputConfig = inputConfig,
+        outputConfig = outputConfig
 
       )
     ), class = c("gcva_batchPredictionJob", "list")
