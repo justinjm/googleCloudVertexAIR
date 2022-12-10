7c959d45eb265e3998372aee84145a8985318b7a
Justin Marciszewski <justin@harborislandanalytics.com>
2022-12-10 15:30:39 -0500

    WIP - batch prediction
    * documenting / rebuilding / reinstalling r package for dev
    * moving to separate vignette for faster iteration
    
    work on #8

--- R/batch_prediction_jobs.R
+++ R/batch_prediction_jobs.R
@@ -5,6 +5,8 @@
 
 
 
+#' Submit a Batch Prediction Job
+#'
 #'
 #' @param jobDisplayName STRING Required. The user-defined name of this BatchPredictionJob.
 #' @param model STRING The name of the Model resource that produces the predictions via this job,
@@ -12,9 +14,16 @@
 #' Exactly one of model and unmanagedContainerModel must be set.
 #' The model resource name may contain version id or version alias to specify the version,
 #' if no version is specified, the default version will be used.
-#' @param bigquerySource BigQuery URI to a table, up to 2000 characters long. Accepted forms: "bq://projectId.bqDatasetId.bqTableId"
-#'
+#' @param bigquerySource string Required. BigQuery URI to a project or table, up to 2000 characters long.
+#' When only the project is specified, the Dataset and Table is created. When the full table reference is specified,
+#' the Dataset must exist and table must not exist.
+#' Accepted forms: BigQuery URI to a table, up to 2000 characters long.
+#' Accepted forms: "bq://projectId.bqDatasetId.bqTableId"
+#' @param instancesFormat
+#' @param predictionsFormat STRING and one of the following: "bigquery"
+#' @param bigqueryDestinationPrefix BigQuery path. For example: bq://projectId or bq://projectId.bqDatasetId or bq://projectId.bqDatasetId.bqTableId.
 #' https://cloud.google.com/vertex-ai/docs/reference/rest/v1/projects.locations.batchPredictionJobs#BatchPredictionJob
+#' @export
 gcva_batch_predict <- function(jobDisplayName,
                                model,
                                bigquerySource,
